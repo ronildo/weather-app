fe6f82a3005120cb30daf68610e494c37c08ed5f
Roni Costa <rcosta@eagleeyeintelligence.com>
2019-02-11 20:25:41 -0500

    [feat]: Add page transition

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,35 @@
   <div id="app">
     <section class="section">
       <div class="container">
-        <router-view />
+
+        <div class="app-container">
+          <transition name="fade">
+            <router-view />
+          </transition>
+        </div>
+
       </div>
     </section>
   </div>
 </template>
 
+<style lang="scss" scoped>
+.section {
+  padding-bottom: 0;
+  padding-top: 0;
+}
+</style>
+
 <style lang="scss">
+@import './assets/scss/transitions';
+
 html {
   background: linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%);
+}
+
+.app-container {
+  border: 1px solid #FF0;
+  padding: 3rem 0;
+  min-height: 100vh;
 }
 </style>
