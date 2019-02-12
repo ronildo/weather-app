6aca094d6a870f1c6358f4597f7088b3dffb7ac4
Roni Costa <rcosta@eagleeyeintelligence.com>
2019-02-11 20:10:50 -0500

    [fix]: Add page container

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <router-view />
+    <section class="section">
+      <div class="container">
+        <router-view />
+      </div>
+    </section>
   </div>
 </template>
 
