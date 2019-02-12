33623cb32df541c0c3f36eb62788194bb9e74eb8
Roni Costa <rcosta@eagleeyeintelligence.com>
2019-02-11 23:52:13 -0500

    [feat]: Page loader

---
+++ src/components/Ui/WLoading/WLoading.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <loading
+      :active.sync="loading"
+      :is-full-page="true"
+      loader="dots"
+      :color="color"
+      :width="size"
+      :height="size" />
+  </div>
+</template>
+
+<script>
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+
+export default {
+  name: 'WLoading',
+
+  components: {
+    Loading
+  },
+
+  props: {
+    loading: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  data: () => ({
+    size: 200,
+    color: '#0079FF'
+  })
+}
+</script>
+
+<style>
+
+</style>
